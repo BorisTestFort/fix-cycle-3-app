6295f50c8c841704194afef0df9608af47adc29a
BorisTestFort <boris.larioshyn.testfort@gmail.com>
2021-09-06 18:07:28 +0300

    changes

---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  data() {
+    return {
+      msg: 'Main branch!!!',
+    };
+  },
+};
+</script>
+
+<style scoped>
+p {
+  color: blue;
+  font-size: 36px;
+}
+
+div {
+  background-color: yellow;
+}
+</style>
